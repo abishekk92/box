c59168fb4f7f07eae8f9d35499fc489841fd3d0f
Abishek Bhat <bhat.abishek@gmail.com>
2012-09-22 17:12:48 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+One click rails installation for dummies.
+Usage:
+`./box.sh and you should be all set.
---
+++ box.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+echo "Downloading dependencies"
+sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+echo "Downloading rvm"
+curl -L https://get.rvm.io | sudo bash -s stable --rails
+
