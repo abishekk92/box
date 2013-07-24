7349ddb65e83e0c7765742c60fe4c3c1d64082be
abishekk92 <bhat.abishek@gmail.com>
2013-07-24 20:20:50 +0530

    Adding to zshrc

--- box.sh
+++ box.sh
@@ -1,8 +1,17 @@
 
-sudo apt-get update
+sudo apt-get -y update
+
 echo "Downloading dependencies"
-sudo apt-get install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+
+sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
+
 echo "Downloading rvm"
+
 curl -L https://get.rvm.io | bash -s stable --rails
+
 echo ". ~/.rvm/scripts/rvm" >> ~/.bashrc
+
+if [-f ~/.zshrc]; then
+    echo ". ~/.rvm/scripts/rvm" >> ~/.zshrc
+fi
 
